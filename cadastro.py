5b40ddb1ef7853e6954ef9edc0f9a37b967301ea
EduardoNRigueira <edununes004@gmail.com>
2026-02-03 20:31:41 -0300

    Adição das funções "remover" e "listar"
    Adição de duas novas funções e alguns ajustes de formatação e tratamento de erro

--- cadastro.py
+++ cadastro.py
@@ -2,7 +2,12 @@ cadastros = []
 
 def cadastrar_pessoa():
     nome = input("Nome: ")
-    idade = input("Idade: ")
+    while True:
+        try:
+            idade = int(input("Idade: "))
+            break
+        except ValueError:
+            print("|Digite uma idade válida|")
     email = input("Email: ")
 
     pessoa = {
@@ -14,16 +19,54 @@ def cadastrar_pessoa():
     cadastros.append(pessoa)
     print("\n|Cadastro realizado com sucesso!|\n")
 
+def remover_cadastro():
+    if not cadastros:
+        print('|Não há cadastro para ser removido!|')
+    else:
+        for i , pessoa in enumerate(cadastros, start = 1):
+            print(f"{i}. Nome: {pessoa['nome']} | Idade: {pessoa['idade']} | Email: {pessoa['email']}")
+        print()
+
+        try:
+            escolha = int(input('Escolha o número do cadastro a ser removido: '))
+
+            if 1 <= escolha <= len(cadastros):
+                removido = cadastros.pop(escolha - 1)
+                print(f"\n|Cadastro de {removido['nome']} removido com sucesso!|\n")
+            else:
+                print('\n|Número inválido!|\n')
+
+        except ValueError:
+            print('\n|Digite um número válido!|')
+
+
+def listar_cadastros():
+    if not cadastros:
+        print('|A lista está vazia!|')
+    else:
+        print("\n|Lista de pessoas cadastradas|")
+        print('=' * 50)
+        for i , pessoa in enumerate(cadastros, start = 1):
+            print(f"{i}. Nome: {pessoa['nome']} | Idade: {pessoa['idade']} | Email: {pessoa['email']}")
+        print('=' * 50)
+        print()
 
 while True:
-    opcao = input("1. Cadastrar pessoa.\n2. Sair.\nDigite o número da sua escolha: ")
+    opcao = input("1. Cadastrar pessoa\n2. Remover cadastro\n3. Lista de cadastros\n4. Sair\nDigite o número da sua escolha: ")
 
     if opcao == "1":
         cadastrar_pessoa()
+    
     elif opcao == "2":
-        print("Programa encerrado")
+        remover_cadastro()
+
+    elif opcao == "3":
+        listar_cadastros()
+
+    elif opcao == "4":
+        print("\nPrograma encerrado")
         break
 
-print(f'Lista de pessoas cadastradas:\n {cadastros}')
+
 
 
